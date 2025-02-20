f0aefde2c0fdcdb88355b1744422c160a7e09809
Isaac <mayne.isaac@gmail.com>
2025-02-20 21:08:20 +0100

    Refactor layout and styling; update metadata and add product pages

---
+++ app/products/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Sidebar from "@/components/Sidebar";
+
+export default function ProductsPage() {
+
+  console.log('desde el servidor');
+  
+  return (
+    <Sidebar/>
+  )
+}
