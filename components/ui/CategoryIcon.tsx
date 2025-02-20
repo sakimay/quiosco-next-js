a25602c48c5c9dc8e39ea2e576f9c48c22e124d8
Isaac <mayne.isaac@gmail.com>
2025-02-20 23:04:39 +0100

    Update CategoryIcon to highlight active category based on URL parameters

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,15 +1,19 @@
-import { Category } from "@prisma/client"
+"use client"
 import Link from "next/link"
 import Image from "next/image"
+import { useParams } from "next/navigation"
+import { Category } from "@prisma/client"
 
 type CategoryIconProps = {
     category: Category
 }
 
 export default function CategoryIcon({ category }: CategoryIconProps) {
+    const params = useParams<{category: string}>()
+    
     return (
         <div
-            className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
+            className={`${category.slug === params.category ? 'bg-amber-400' : ''} flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
             <div className="w-16 h-16 relative">
                 <Image
                     fill
