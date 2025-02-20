5435fefd5a2245f035e67594afdac33c14fb21b7
Isaac <mayne.isaac@gmail.com>
2025-02-20 21:17:56 +0100

    Add order layout and components; update products page structure

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,10 @@
-"use client";
-import Sidebar from "@/components/Sidebar";
+
 
 export default function ProductsPage() {
 
   console.log('desde el servidor');
   
   return (
-    <Sidebar/>
+    <h1></h1>
   )
 }
---
+++ components/order/OrderSidebar.tsx
@@ -0,0 +1,9 @@
+
+export default function OrderSidebar() {
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+        OrderSidebar
+
+    </aside>
+  )
+}
