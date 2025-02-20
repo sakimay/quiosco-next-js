6e33689570649a5b3ebad1d1b5e5afd7c318f4b5
Isaac <mayne.isaac@gmail.com>
2025-02-20 22:57:52 +0100

    Add header to OrderPage and implement product images in ProductCard component

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -18,6 +18,8 @@ export default async function OrderPage({ params }: { params: { category: string
 
   return (
     <>
+      <h1 className="text-2xl my-10">Elige y personaliza tu pedido a continuación</h1>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
         {products.map((product) => (
           <ProductCard
--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,6 @@
 import { formatCurrency } from "@/src/utils"
 import { Product } from "@prisma/client"
+import Image from "next/image"
 
 type ProductCardProps = {
     product: Product
@@ -7,12 +8,25 @@ type ProductCardProps = {
 export default function ProductCard({ product } : ProductCardProps) {
     return (
         <div className="border bg-white">
+
+            <Image
+                src={`/products/${product.image}.jpg`}
+                alt={`Imagen ${product.name}`}
+                width={400}
+                height={500}
+            />
+
             <div className="p-5">
                 <h3 className="text-2xl font-bold">{product.name}</h3>
                 <p className="mt-5 font-black text-4xl text-amber-500">
                     { formatCurrency(product.price) }
                 </p>
+                <button
+                    type="button"
+                    className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+                >Agregar</button>
             </div>
+
         </div>
     )
 }
