6acb2799e11e17baf44491415541447d7c1fa1e1
Isaac <mayne.isaac@gmail.com>
2025-03-05 23:32:50 +0100

    add search

---
+++ app/admin/products/search/page.tsx
@@ -0,0 +1,35 @@
+import ProductSearchForm from "@/components/products/ProductSearchForm";
+import ProductTable from "@/components/products/ProductTable";
+import Heading from "@/components/ui/Heading";
+import { prisma } from "@/src/lib/prisma";
+
+async function searchProducts(searchTerm: string) {
+    const products = await prisma.product.findMany({
+        where: {
+            name: {
+                contains: searchTerm,
+                mode: 'insensitive'
+            }
+        },
+        include: {
+            category: true
+        }
+    })
+
+    return products
+}
+
+export default async function SearchPage({ searchParams }: { searchParams: { search: string } }) {
+
+    const products = await searchProducts(searchParams.search)
+
+    return (
+        <>
+            <Heading>Resultados de busqueda</Heading>
+            <div className="flex flex-col gap-5 lg:flex-row lg:justify-end">
+                <ProductSearchForm />
+            </div>
+            <ProductTable products={products} />
+        </>
+    )
+}
--- components/products/ProductSearchForm.tsx
+++ components/products/ProductSearchForm.tsx
@@ -2,22 +2,24 @@
 import { SearchSchema } from "@/src/schema"
 import { toast } from "react-toastify"
 import { redirect } from "next/navigation"
+import { useRouter } from "next/navigation"
 
 export default function ProductSearchForm() {
+    const router = useRouter()
 
     const handleSearchForm = (formData: FormData) => {
         const data = {
             search: formData.get('search')
         }
         const result = SearchSchema.safeParse(data)
-        
+
         if (!result.success) {
             result.error.issues.forEach(issue => {
                 toast.error(issue.message)
             })
             return
         }
-        redirect(`/admin/products?search=${result.data.search}`)
+        router.push(`/admin/products/search?search=${result.data.search}`)
     }
 
     return (
